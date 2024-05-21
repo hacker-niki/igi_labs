5ee3351a2c1a5cb4861c361784fc9deb8394e9c5
hacker_niki <nktsenko@gmail.com>
2024-05-21 23:50:08 +0300

    registration

--- IGI/LR5/pizzeria/pizzeria/urls.py
+++ IGI/LR5/pizzeria/pizzeria/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.urls import re_path
+
+from . import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('pizza.urls')),
+                  path('registration/', include('registration.urls')),
+                  # path('media', )
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
